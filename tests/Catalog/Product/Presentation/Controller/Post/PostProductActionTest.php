0eeef8b0df9d9aa3bb7c234d0449ea4e5f000589
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-10-26 16:31:39 -0400

    adding tests deps

---
+++ tests/Catalog/Product/Presentation/Controller/Post/PostProductActionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Catalog\Product\Presentation\Controller\Post;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PostProductActionTest extends WebTestCase
+{
+    private readonly KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testCreateProduct(): void
+    {
+        $this->client->jsonRequest('POST', '/products', [
+            'id' => '018b6d1a-5d95-7461-bb15-61544739c92a',
+            'name' => 'Product 1',
+            'description' => 'Product 1 description',
+        ]);
+
+        $expected = [
+            'id' => '018b6d1a-5d95-7461-bb15-61544739c92a',
+            'name' => 'Product 1',
+            'description' => 'Product 1 description',
+            'createdAt' => '@is_string_datetime',
+        ];
+
+        self::assertSame($expected, $this->client->getInternalResponse()->toArray());
+    }
+}
