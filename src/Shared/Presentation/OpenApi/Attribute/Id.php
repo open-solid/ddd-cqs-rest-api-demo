42bddb0ae1a5b3ab8b2579f21251d28674c02c29
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-11-14 09:57:36 -0500

    simplify path extension

--- src/Shared/Presentation/OpenApi/Attribute/Id.php
+++ src/Shared/Presentation/OpenApi/Attribute/Id.php
@@ -7,11 +7,6 @@ use Yceruto\OpenApiBundle\Attribute\Path;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 class Id extends Path
 {
-    public function defaults(): array
-    {
-        return [
-            'format' => 'uuid',
-            'example' => 'f81d4fae-7dec-11d0-a765-00a0c91e6bf9',
-        ];
-    }
+    public $_format = 'uuid';
+    public $_example = 'f81d4fae-7dec-11d0-a765-00a0c91e6bf9';
 }
