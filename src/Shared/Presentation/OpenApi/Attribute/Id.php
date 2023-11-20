24d7ed7fd92cdb37e30b0395d5276844f11a58a9
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-11-19 19:28:33 -0500

    update way to extend Path attribute

--- src/Shared/Presentation/OpenApi/Attribute/Id.php
+++ src/Shared/Presentation/OpenApi/Attribute/Id.php
@@ -3,10 +3,16 @@
 namespace App\Shared\Presentation\OpenApi\Attribute;
 
 use Yceruto\OpenApiBundle\Attribute\Path;
+use Yceruto\OpenApiBundle\Attribute\PathDefaults;
 
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 class Id extends Path
 {
-    public $_format = 'uuid';
-    public $_example = 'f81d4fae-7dec-11d0-a765-00a0c91e6bf9';
+    public static function defaults(): PathDefaults
+    {
+        return PathDefaults::create()
+            ->format('uuid')
+            ->example('f81d4fae-7dec-11d0-a765-00a0c91e6bf9')
+        ;
+    }
 }
