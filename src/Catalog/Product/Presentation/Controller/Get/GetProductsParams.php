27a41936b917f5743467f9978b1eb79cd097962c
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-11-28 11:39:06 -0500

    Fix support for query parameters

--- src/Catalog/Product/Presentation/Controller/Get/GetProductsParams.php
+++ src/Catalog/Product/Presentation/Controller/Get/GetProductsParams.php
@@ -2,10 +2,12 @@
 
 namespace App\Catalog\Product\Presentation\Controller\Get;
 
+use OpenSolid\OpenApiBundle\Attribute\Param;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class GetProductsParams
 {
+    #[Param]
     #[Assert\Length(min: 3, max: 255)]
     public ?string $name = null;
 }
